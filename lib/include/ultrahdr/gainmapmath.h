790ec9ce9a12894c81e1e0818e672651780959fa
DichenZhang1 <140119224+DichenZhang1@users.noreply.github.com>
2024-03-05 00:30:41 -0800

    Update gainmapmath.h

--- lib/include/ultrahdr/gainmapmath.h
+++ lib/include/ultrahdr/gainmapmath.h
@@ -29,7 +29,8 @@ namespace ultrahdr {
 ////////////////////////////////////////////////////////////////////////////////
 // Framework
 
-const float kSdrWhiteNits = 100.0f;
+// ISO 22028-5
+const float kSdrWhiteNits = 203.0f;
 const float kHlgMaxNits = 1000.0f;
 const float kPqMaxNits = 10000.0f;
 
